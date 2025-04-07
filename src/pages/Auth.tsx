c235f643cf226e9306f37d0dcf59febde15b9e0d
joaohunntz <140175010+joaohunntz@users.noreply.github.com>
2025-04-07 17:58:54 -0300

    callback

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -49,7 +49,7 @@ const Auth = () => {
       const { error } = await supabase.auth.signInWithOtp({
         email,
         options: {
-          emailRedirectTo: `https://oab-flashcard-ace.vercel.app/home`,
+          emailRedirectTo: `https://oab-flashcard-ace.vercel.app/callback`,
         }
       });
 
